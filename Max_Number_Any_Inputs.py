1e7ffe694104ff54f48719bdf3322b91449b9f5b
Ramshiv7 <67915300+Ramshiv7@users.noreply.github.com>
2020-07-07 22:34:31 +0530

    Create Max_Number_Any_Inputs.py

---
+++ Max_Number_Any_Inputs.py
@@ -0,0 +1,7 @@
+#To find max number for any user input 
+# Note - Double Digit Values should be seperated by "," - sample - 10000,9999,888 
+
+number=input()
+numb=number.split(',')
+
+print(max(numb))
