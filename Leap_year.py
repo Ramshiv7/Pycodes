505d2fde04e0cda2d3da463f8e49521a55a63f61
Ramshiv7 <67915300+Ramshiv7@users.noreply.github.com>
2020-07-09 21:30:31 +0530

    Create Leap_year.py

---
+++ Leap_year.py
@@ -0,0 +1,12 @@
+# Leap Year Using Function - HackerRank Solution
+
+year=int(input())
+
+def is_leap(year):
+    leap = False
+    if year%4==0 and (year%400==0 or year%100!=0):
+        leap = True
+    return leap
+
+
+print(is_leap(year))
