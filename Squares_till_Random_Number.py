6b3ed036ed8825ee70b0e3100440346f903ea413
Ramshiv7 <67915300+Ramshiv7@users.noreply.github.com>
2020-07-12 17:17:42 +0530

    Create Squares_till_Random_Number.py

---
+++ Squares_till_Random_Number.py
@@ -0,0 +1,13 @@
+# Printing Squares till a Randomly Choosen Numbers
+
+import random
+
+
+x = random.randint(0,100)
+print(f'Py will print Square of the Values till {x-1}')
+print('''
+
+''')
+
+for i in range(x):
+    print(i*i)
